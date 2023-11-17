0b59132347167de1ba025f14b1a7baaae4c071af
Giovanni Bruno <giovanni.bruno@cyberneid.com>
2023-11-17 12:09:28 +0100

    Indentation

--- lib/parse_helper.dart
+++ lib/parse_helper.dart
@@ -1,17 +1,13 @@
 library parse_helper;
 
-
 import 'package:parse_server_sdk_flutter/parse_server_sdk_flutter.dart';
 
-
-
 class ParseHelper {
-
-  static Future<ParseObject> fetchParseObjectFromObjId(String oid,String classname) async {
-
+  static Future<ParseObject> fetchParseObjectFromObjId(
+      String oid, String classname) async {
     final QueryBuilder<ParseObject> parseQuery =
-    QueryBuilder<ParseObject>(ParseObject(classname));
-    parseQuery.whereEqualTo('objectId',oid);
+        QueryBuilder<ParseObject>(ParseObject(classname));
+    parseQuery.whereEqualTo('objectId', oid);
 
     final ParseResponse apiResponse = await parseQuery.query();
     if (apiResponse.success && apiResponse.results != null) {
@@ -21,16 +17,15 @@ class ParseHelper {
     return new ParseObject(classname);
   }
 
-
-  static Future<List<ParseObject>> fetchListParseObjectFromObjId(String classname,String column,String value) async {
-
+  static Future<List<ParseObject>> fetchListParseObjectFromObjId(
+      String classname, String column, String value) async {
     final QueryBuilder<ParseObject> parseQuery =
-    QueryBuilder<ParseObject>(ParseObject(classname));
-    parseQuery.whereEqualTo(column,value);
+        QueryBuilder<ParseObject>(ParseObject(classname));
+    parseQuery.whereEqualTo(column, value);
 
     final ParseResponse apiResponse = await parseQuery.query();
     if (apiResponse.success && apiResponse.results != null) {
-      if(apiResponse.results != null && apiResponse.results!.length > 0) {
+      if (apiResponse.results != null && apiResponse.results!.length > 0) {
         return apiResponse.results as List<ParseObject>;
       }
     }
@@ -38,46 +33,37 @@ class ParseHelper {
     return emptyList;
   }
 
-
-
   //* classname => The name of parse Class
   //* conditions => a Map of condition where/equalTo
   //* includes => a list of class included in my record for inner visibility of objects
 
-  static Future<List<ParseObject>> fetchListParseObjectWithCondition(String classname ,Map<String,dynamic>? conditions,List<String>? includes) async {
-
-
+  static Future<List<ParseObject>> fetchListParseObjectWithCondition(
+      String classname,
+      Map<String, dynamic>? conditions,
+      List<String>? includes) async {
     final QueryBuilder<ParseObject> parseQuery =
+        QueryBuilder<ParseObject>(ParseObject(classname));
 
-      
-    QueryBuilder<ParseObject>(ParseObject(classname ));
-
-    if(conditions != null) {
+    if (conditions != null) {
       //Add where Condition
       conditions.forEach((key, value) {
         parseQuery.whereEqualTo(key, value);
       });
-
     }
     //Add IncludeObject
-    if(includes != null) {
-      if(includes.isNotEmpty) {
+    if (includes != null) {
+      if (includes.isNotEmpty) {
         parseQuery.includeObject(includes);
       }
     }
 
     final ParseResponse apiResponse = await parseQuery.query();
     if (apiResponse.success && apiResponse.results != null) {
-      if(apiResponse.results != null && apiResponse.results!.length > 0) {
+      if (apiResponse.results != null && apiResponse.results!.length > 0) {
         return apiResponse.results as List<ParseObject>;
       }
     }
     List<ParseObject> emptyList = [];
     return emptyList;
   }
-  
 }
-
-
-
-
